67d3eea2bfcec62e0466f2ac73cf1fabd31ce523
utkarsh <r.utkarsh.0010@gmail.com>
2024-11-05 12:31:51 +0530

    add auth layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+
+interface AuthLayoutProps {
+  children: ReactNode;
+}
+
+const AuthLayout = ({ children } : AuthLayoutProps) => {
+  return (
+    <div className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <Image src="/logo/heera.png" width={152} height={56} alt="Logo" />
+          </div>
+        </nav>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default AuthLayout;
