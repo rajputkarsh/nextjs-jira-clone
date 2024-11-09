6fac564677e11290adaffecd4cd567a6fb932f8d
utkarsh <r.utkarsh.0010@gmail.com>
2024-11-09 20:38:38 +0530

    replace default export with named export

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import SignInCard from "@/features/auth/components/sign-in-card";
+import { SignInCard } from "@/features/auth/components/sign-in-card";
 
 function SignIn() {
   return <SignInCard />;
