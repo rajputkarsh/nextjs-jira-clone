af02b95c573b2c8a5a2eb378b9adf5c0e3aa5f7b
utkarsh <r.utkarsh.0010@gmail.com>
2024-11-14 20:28:47 +0530

    link auth routes on navbar

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,35 @@
+'use client';
+
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 interface AuthLayoutProps {
   children: ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+
+  const translations = useTranslations("auth");
+  const pathname = usePathname();
+
+  const isSignIn = pathname === "/sign-in";
+
   return (
     <div className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-3xl p-4">
         <nav className="flex justify-between items-center">
           <Image src="/logo/heera.png" width={152} height={56} alt="Logo" />
-          <Button variant="secondary">Sign Up</Button>
+          <Button asChild variant="secondary">
+            <Link href={ isSignIn ? "sign-up" : "sign-in"}>
+              { isSignIn
+                ? translations("sign_up")
+                : translations("login")}
+            </Link>
+          </Button>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
           {children}
