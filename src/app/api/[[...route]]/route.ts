3250d1b5a6b93b0fca453936d446236336e6af88
utkarsh <r.utkarsh.0010@gmail.com>
2024-11-28 15:09:22 +0530

    add hono API routes

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,8 +3,9 @@ import { handle } from "hono/vercel";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({ hello: "hello" });
+app.get("/project/:projectId", (c) => {
+  const { projectId } = c.req.param()
+  return c.json({ projectId });
 });
 
 export const GET = handle(app);
