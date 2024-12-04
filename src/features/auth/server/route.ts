60cb11b00c9ee1522e56d880fc00e46f7a675a6a
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-04 23:58:10 +0530

    update sign-up API to create user in appwrite

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,13 +1,19 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { SignInFormSchema, SignUpFormSchema } from "../schema";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
 
 const app = new Hono()
-  .post("/sign-in", zValidator("json", SignInFormSchema), (c) => {
+  .post("/sign-in", zValidator("json", SignInFormSchema), async (c) => {
     return c.json({ success: "ok" });
   })
-  .post("/sign-up", zValidator("json", SignUpFormSchema), (c) => {
+  .post("/sign-up", zValidator("json", SignUpFormSchema), async (c) => {
     const { name, email, password } = c.req.valid('json');
+
+    const { account } = await createAdminClient();
+    const user = await account.create(ID.unique(), email, password, name)
+
     return c.json({ success: "ok" });
   });
 
