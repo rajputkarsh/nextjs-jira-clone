da5f05c5a6af9647112e960680175b0e78e6b2ca
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-05 08:30:49 +0530

    implement logout

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -4,10 +4,9 @@ import { InferRequestType, InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
 
 type ResponseType = InferResponseType<typeof client.api.auth['logout']['$post']>;
-type RequestType = InferRequestType<typeof client.api.auth['logout']['$post']>;
 
 export const useLogout = () => {
-  const mutation = useMutation<ResponseType, Error, RequestType>({
+  const mutation = useMutation<ResponseType, Error>({
     mutationFn: async () => {
       const response = await client.api.auth["logout"]["$post"]();
       return await response.json();
