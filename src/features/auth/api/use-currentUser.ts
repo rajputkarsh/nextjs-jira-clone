f92a3cfea21eaeb1ec039ef10b67bce8fc02b4d7
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-08 17:17:18 +0530

    fix useCurrentUser hook

--- src/features/auth/api/use-currentUser.ts
+++ src/features/auth/api/use-currentUser.ts
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
 export const useCurrentUser = () => {
+  const router = useRouter();
   const mutation = useQuery({
     queryKey: ["current"],
     queryFn: async () => {
       const response = await client.api.auth["current-user"].$get();
 
       if(!response.ok || response.status >= 400) {
+        router.push('/sign-in')
         return null;
       }
 
