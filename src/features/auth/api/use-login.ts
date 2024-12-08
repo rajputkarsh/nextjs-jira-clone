a8f07240cde44a47e1a624e0807ee8544e44bd6f
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-09 03:46:42 +0530

    fix login redirections

--- src/features/auth/api/use-login.ts
+++ src/features/auth/api/use-login.ts
@@ -1,17 +1,26 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.auth['sign-in']['$post']>;
 type RequestType = InferRequestType<typeof client.api.auth['sign-in']['$post']>;
 
 export const useLogin = () => {
+
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json }) => {
       const response = await client.api.auth["sign-in"]["$post"]({ json });
       return await response.json();
-    }
+    },
+    onSuccess: () => {
+      router.refresh();
+      queryClient.invalidateQueries({ queryKey: ["current"] });
+    },
   });
 
   return mutation;
