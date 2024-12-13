52762146a139087aa522bec2d6a6702e8e10624f
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-13 05:34:05 +0530

    trigger mobile sidebar change with pathname

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -1,23 +1,33 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
+import { usePathname } from "next/navigation";
 
 function MobileSidebar() {
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathName])
+
   return (
-    <Sheet modal={false}>
+    <Sheet modal={false} open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild>
         <Button variant={"secondary"} className="lg:hidden">
           <MenuIcon className="size-4 text-neutral-500" />
         </Button>
       </SheetTrigger>
       <SheetContent side={"left"} className="p-0">
-        <Sidebar/>
+        <Sidebar />
       </SheetContent>
     </Sheet>
-  )
+  );
 }
 
 export default MobileSidebar;
