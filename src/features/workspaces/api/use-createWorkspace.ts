3534b224289b1a5c9f849b175aa7505785b96b0f
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-15 00:37:03 +0530

    feature: workspace - add useCreateWorkspace mmutation

--- src/features/workspaces/api/use-createWorkspace.ts
+++ src/features/workspaces/api/use-createWorkspace.ts
@@ -2,14 +2,12 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
-import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.workspaces['$post']>;
 type RequestType = InferRequestType<typeof client.api.workspaces['$post']>;
 
 export const useCreateWorkspace = () => {
 
-  const router = useRouter();
   const queryClient = useQueryClient();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
@@ -18,7 +16,6 @@ export const useCreateWorkspace = () => {
       return await response.json();
     },
     onSuccess: () => {
-      router.refresh();
       queryClient.invalidateQueries({ queryKey: ["workspaces"] });
     },
   });
