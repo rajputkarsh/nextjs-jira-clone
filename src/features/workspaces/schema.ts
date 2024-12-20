ab97a0331c364fe57613c21eaf1d91dcc7eb6634
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-20 05:54:27 +0530

    update createWorkspaceSchema

--- src/features/workspaces/schema.ts
+++ src/features/workspaces/schema.ts
@@ -3,12 +3,18 @@ import { z } from 'zod';
 
 export interface ICreateWorkspace {
   name: string;
+  imageUrl?: string;
 }
 
 export const createWorkspaceSchema = z.object({
   name: z.string().trim().min(1, "Required"),
+  imageUrl: z.union([
+    z.instanceof(File),
+    z.string().transform((value) => value === "" ? undefined : value)
+  ]).optional(),
 });
 
 export const createWorkspaceFormDefaultValues: ICreateWorkspace = {
   name: "",
+  imageUrl: "",
 };
