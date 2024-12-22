b981f714e488485e323a744aac6883f0a1bc7a0c
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-22 08:25:32 +0530

    add new query to fetch list of workspaces

---
+++ src/features/workspaces/api/use-getWorkspaces.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { useTranslations } from "next-intl";
+import { client } from "@/lib/rpc";
+
+export const useCreateWorkspace = () => {
+  const translations = useTranslations("CreateWorkspaceForm");
+  const query = useQuery({
+    queryKey: ["workspaces"],
+    queryFn: async () => {
+      const response = await client.api.workspaces.$get();
+
+      if (!response.ok || response.status >= 400) {
+        throw new Error("failed_to_fetch_workspaces");
+      }
+
+      const { data } = await response.json();
+      return data;
+    },
+  });
+
+  return query;
+};
