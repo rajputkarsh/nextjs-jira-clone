d836c3b336c8edecdaad1e6c9ab2c590c347bfee
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-29 10:07:30 +0530

    fix create-workspace modal issues

--- src/components/responsive-modal.tsx
+++ src/components/responsive-modal.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useMedia } from "react-use";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Drawer, DrawerContent } from "@/components/ui/drawer";
---
+++ src/features/workspaces/components/CreateWorkspaceModal/index.tsx
@@ -0,0 +1,12 @@
+import ResponsiveModal from "@/components/responsive-modal";
+import CreateWorkSpaceForm from "../CreateWorkspaceForm";
+
+function CreateWorkspaceModal() {
+  return (
+    <ResponsiveModal open={true} onOpenChange={() => {}}>
+      <CreateWorkSpaceForm />
+    </ResponsiveModal>
+  )
+}
+
+export default CreateWorkspaceModal
