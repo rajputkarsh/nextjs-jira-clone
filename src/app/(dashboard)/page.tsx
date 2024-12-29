4272b36c90f9ab1988367fcf5f9399b5571b2994
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-29 07:02:34 +0530

    homepage: update workspace redirections

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,17 +1,17 @@
 import { getCurrent } from "@/features/auth/actions";
-import UserButton from "@/features/auth/components/UserButton";
+import { getWorkspaces } from "@/features/workspaces/actions";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await getCurrent();
+  if (!user) redirect("/sign-in");
 
-  if (!user) {
-    redirect("/sign-in");
+  const workspaces = await getWorkspaces();
+
+  if (workspaces.total === 0) {
+    redirect('/workspaces/create');
+  } else {
+    redirect(`/workspaces/${workspaces.documents?.[0]?.$id}`);
   }
 
-  return (
-    <div className="">
-      Home page content
-    </div>
-  );
 }
