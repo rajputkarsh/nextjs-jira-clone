2f8073e10c29948e566dc59fff473bdcac8ddc36
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-29 06:51:20 +0530

    update workspace switcher to redirect to workspace

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -11,10 +11,18 @@ import {
   SelectValue
 } from "@/components/ui/select";
 import WorkspaceAvatar from "@/features/workspaces/components/WorkspaceAvatar";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspaceId";
 
 function WorkspaceSwitcher() {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
   const translations = useTranslations("WorkspaceSwitcher");
   const { data: workspaces } = useGetWorkspace();
+
+  const handleSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  }
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -24,7 +32,7 @@ function WorkspaceSwitcher() {
         </p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
-      <Select>
+      <Select onValueChange={handleSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
           <SelectValue placeholder={translations("no_workspace_selected")} />
         </SelectTrigger>
