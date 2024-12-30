7d9e1dc1cee9fa90e97090f821032b10e04c2c21
utkarsh <r.utkarsh.0010@gmail.com>
2024-12-30 14:39:29 +0530

    add authentication to pages

--- src/app/(standalone)/workspaces/create/page.tsx
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -1,6 +1,11 @@
+import { redirect } from "next/navigation";
+import { getCurrent } from "@/features/auth/actions";
 import CreateWorkSpaceForm from "@/features/workspaces/components/CreateWorkspaceForm"
 
-function CreateWorkspace() {
+async function CreateWorkspace() {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
   return (
     <div className="w-full lg:max-w-xl">
       <CreateWorkSpaceForm />
