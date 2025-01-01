619239a47da1a2ed938bf998d73ac720abcaf61d
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-01 15:04:57 +0530

    workspaces: add patch API

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -3,43 +3,36 @@ import { ID, Query } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
 import { sessionMiddleware } from "@/middlewares/session";
 import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
-import { createWorkspaceSchema } from "../schema";
+import { createWorkspaceSchema, updateWorkspaceSchema } from "../schema";
 import { MemberRole } from "@/features/members/types";
 import { generateInviteCode } from "@/lib/utils";
 import { WORKSPACE_INVITE_CODE_LENGTH } from "../constants";
+import { getMembers } from "@/features/members/types/utils";
+import { HTTP_STATUS } from "@/constants/api";
 
 const app = new Hono()
-  .get(
-    "/",
-    sessionMiddleware,
-    async (c) => {
-      const user = c.get("user");
-      const databases = c.get("databases");
+  .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user");
+    const databases = c.get("databases");
 
-      const members = await databases.listDocuments(
-        DATABASE_ID,
-        MEMBERS_ID,
-        [Query.equal('userId', user.$id)]
-      );
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal("userId", user.$id),
+    ]);
 
-      if (!members.documents.length) {
-        return c.json({ data: { documents: [], total: 0 } });
-      }
-
-      const workspaceIds = members.documents.map((member) => member.workspaceId);
-
-      const workspaces = await databases.listDocuments(
-        DATABASE_ID,
-        WORKSPACES_ID,
-        [
-          Query.contains("$id", workspaceIds),
-          Query.orderDesc("$createdAt")
-        ]
-      );
-
-      return c.json({ data: workspaces });
+    if (!members.documents.length) {
+      return c.json({ data: { documents: [], total: 0 } });
     }
-  )
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+    const workspaces = await databases.listDocuments(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      [Query.contains("$id", workspaceIds), Query.orderDesc("$createdAt")]
+    );
+
+    return c.json({ data: workspaces });
+  })
   .post(
     "/",
     zValidator("form", createWorkspaceSchema),
@@ -82,16 +75,61 @@ const app = new Hono()
         }
       );
 
-      await databases.createDocument(
+      await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+        userId: user.$id,
+        workspaceId: workspace.$id,
+        role: MemberRole.ADMIN,
+      });
+
+      return c.json({ data: workspace });
+    }
+  )
+  .patch(
+    "/:workspaceId",
+    zValidator("form", updateWorkspaceSchema),
+    sessionMiddleware,
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
+
+      const { workspaceId } = c.req.param();
+      const { name, image } = c.req.valid("form");
+
+      const member = await getMembers({ databases, workspaceId, userId: user?.$id });
+
+      if (!member || member?.role !== MemberRole.ADMIN) {
+        return c.json({ error: HTTP_STATUS.UNAUTHORISED.MESSAGE }, HTTP_STATUS.UNAUTHORISED.STATUS);
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`;
+      }
+
+      const workspace = await databases.updateDocument(
         DATABASE_ID,
-        MEMBERS_ID,
-        ID.unique(),
+        WORKSPACES_ID,
+        workspaceId,
         {
-          userId: user.$id,
-          workspaceId: workspace.$id,
-          role: MemberRole.ADMIN
+          name,
+          imageUrl: uploadedImageUrl,
         }
-      )
+      );
 
       return c.json({ data: workspace });
     }
