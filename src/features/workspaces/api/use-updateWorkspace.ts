6f7921af06bf58c90ea1d56642fb2f0a5a812d4b
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-01 15:15:11 +0530

    workspaces: add use-updateWorkspace

---
+++ src/features/workspaces/api/use-updateWorkspace.ts
@@ -0,0 +1,41 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { useTranslations } from "next-intl";
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<typeof client.api.workspaces[":workspaceId"]['$patch'], 200>;
+type RequestType = InferRequestType<
+  (typeof client.api.workspaces)[":workspaceId"]["$patch"]
+>;
+
+export const useUpdateWorkspace = () => {
+
+  const translations = useTranslations("UpdateWorkspaceForm");
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ form, param }) => {
+      const response = await client.api.workspaces[":workspaceId"]["$patch"]({
+        form,
+        param
+      });
+
+      if (!response.ok) {
+        throw new Error(translations("failed_to_update_workspace"));
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success(translations("workspace_updated"));
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+      queryClient.invalidateQueries({ queryKey: ["workspace", data?.$id] });
+    },
+    onError: () => {
+      toast.error(translations("failed_to_update_workspace"));
+    }
+  });
+
+  return mutation;
+}
