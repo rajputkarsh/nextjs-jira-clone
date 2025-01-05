7fdb0f3dd7983b57ca7c5a8640c15adb490c4754
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-05 23:36:04 +0530

    members: add useGetMembers query

---
+++ src/features/members/api/use-getMembers.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { client } from "@/lib/rpc";
+
+interface UseGetMembersProps {
+  workspaceId: string;
+}
+
+export const useGetMembers = ({ workspaceId }: UseGetMembersProps) => {
+  const query = useQuery({
+    queryKey: ["members"],
+    queryFn: async () => {
+      const response = await client.api.members.$get({ query: { workspaceId } });
+
+      if (!response.ok || response.status >= 400) {
+        throw new Error("failed_to_fetch_members");
+      }
+
+      const { data } = await response.json();
+      return data;
+    },
+  });
+
+  return query;
+};
