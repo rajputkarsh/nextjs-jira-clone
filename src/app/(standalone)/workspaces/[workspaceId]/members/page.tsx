8231d74231d23a50957cf63f8b9c322627f08fce
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-05 23:40:47 +0530

    add workspace members page

---
+++ src/app/(standalone)/workspaces/[workspaceId]/members/page.tsx
@@ -0,0 +1,37 @@
+import { getCurrent } from "@/features/auth/queries";
+import { getWorkspaceInfo } from "@/features/workspaces/queries";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface WorkspaceMembersPageProps {
+  params: {
+    workspaceId: string;
+    inviteCode: string;
+  };
+}
+
+async function WorkspaceMembers({ params }: WorkspaceMembersPageProps) {
+  const user = await getCurrent();
+  if (!user) {
+    const headersList = headers();
+    const host = headersList.get("host") || "";
+    const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+
+    // Construct the current URL
+    const currentUrl = `${protocol}://${host}/workspaces/${params.workspaceId}/join/${params.inviteCode}`;
+    const encodedCallbackUrl = encodeURIComponent(currentUrl);
+    redirect(`/sign-in?callbackUrl=${encodedCallbackUrl}`);
+  }
+  
+    const workspace = await getWorkspaceInfo({ workspaceId: params.workspaceId });
+  
+    if (!workspace) {
+      redirect("/");
+    }
+
+  return (
+    <div>WorkspaceMembers</div>
+  )
+}
+
+export default WorkspaceMembers
