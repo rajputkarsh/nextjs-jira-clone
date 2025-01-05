907b9e90345831e576e0911eb67021138d88d03f
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-05 23:26:18 +0530

    members: add patch API

--- src/features/members/server/route.ts
+++ src/features/members/server/route.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { z } from "zod";
 import { sessionMiddleware } from "@/middlewares/session";
 import { zValidator } from "@hono/zod-validator";
 import { createAdminClient } from "@/lib/appwrite";
@@ -8,13 +7,14 @@ import { HTTP_STATUS } from "@/constants/api";
 import { DATABASE_ID, MEMBERS_ID } from "@/config";
 import { Query } from "node-appwrite";
 import { MemberRole } from "../types";
+import { getMembersListSchema, patchMemberSchema } from "../schema";
 
 const app = new Hono()
   .get(
     "/",
     sessionMiddleware,
-    zValidator("query", z.object({ workspaceId: z.string().trim() })),
-     async (c) => {
+    zValidator("query", getMembersListSchema),
+    async (c) => {
       const { users } = await createAdminClient();
       const databases = c.get("databases");
       const user = c.get("user");
@@ -24,23 +24,19 @@ const app = new Hono()
       const member = await getMembers({
         databases,
         workspaceId,
-        userId: user.$id
+        userId: user.$id,
       });
 
-      if(!member) {
+      if (!member) {
         return c.json(
           { error: HTTP_STATUS.UNAUTHORISED.MESSAGE },
           HTTP_STATUS.UNAUTHORISED.STATUS
         );
       }
 
-      const members = await databases.listDocuments(
-        DATABASE_ID, 
-        MEMBERS_ID, 
-        [
-          Query.equal("workspaceId", workspaceId),
-        ]
-      );
+      const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+        Query.equal("workspaceId", workspaceId),
+      ]);
 
       const populatedMembers = await Promise.all(
         members.documents.map(async (member) => {
@@ -50,22 +46,63 @@ const app = new Hono()
             ...member,
             name: user.name,
             email: user.email,
-          }
+          };
         })
       );
 
       return c.json({ data: { ...members, documents: populatedMembers } });
-     }
+    }
   )
-  .delete(
+  .delete("/:memberId", sessionMiddleware, async (c) => {
+    const { memberId } = c.req.param();
+    const user = c.get("user");
+    const databases = c.get("databases");
+
+    const memberToDelete = await databases.getDocument(
+      DATABASE_ID,
+      MEMBERS_ID,
+      memberId
+    );
+
+    const allMembersInWorkspace = await databases.listDocuments(
+      DATABASE_ID,
+      MEMBERS_ID,
+      [Query.equal("workspaceId", memberToDelete.workspaceId)]
+    );
+
+    const member = await getMembers({
+      databases,
+      workspaceId: memberToDelete.workspaceId,
+      userId: user.$id,
+    });
+
+    if (
+      !member ||
+      allMembersInWorkspace.total < 2 ||
+      (member.$id !== memberToDelete.$id && member.role !== MemberRole.ADMIN)
+    ) {
+      return c.json(
+        { error: HTTP_STATUS.UNAUTHORISED.MESSAGE },
+        HTTP_STATUS.UNAUTHORISED.STATUS
+      );
+    }
+
+    await databases.deleteDocument(DATABASE_ID, MEMBERS_ID, memberId);
+
+    return c.json({ data: { $id: memberId } });
+  })
+  .patch(
     "/:memberId",
     sessionMiddleware,
+    zValidator("json", patchMemberSchema),
     async (c) => {
       const { memberId } = c.req.param();
+      const { role } = c.req.valid("json");
+
       const user = c.get("user");
       const databases = c.get("databases");
 
-      const memberToDelete = await databases.getDocument(
+      const memberToUpdate = await databases.getDocument(
         DATABASE_ID,
         MEMBERS_ID,
         memberId
@@ -74,31 +111,36 @@ const app = new Hono()
       const allMembersInWorkspace = await databases.listDocuments(
         DATABASE_ID,
         MEMBERS_ID,
-        [
-          Query.equal("workspaceId", memberToDelete.workspaceId)
-        ]
+        [Query.equal("workspaceId", memberToUpdate.workspaceId)]
       );
 
       const member = await getMembers({
         databases,
-        workspaceId: memberToDelete.workspaceId,
+        workspaceId: memberToUpdate.workspaceId,
         userId: user.$id,
       });
 
-      if(!member || (member.$id !== memberToDelete.$id && member.role !== MemberRole.ADMIN)) {
+      if (
+        !member ||
+        allMembersInWorkspace.total < 2 ||
+        (member.role !== MemberRole.ADMIN)
+      ) {
         return c.json(
           { error: HTTP_STATUS.UNAUTHORISED.MESSAGE },
           HTTP_STATUS.UNAUTHORISED.STATUS
         );
       }
 
-      await databases.deleteDocument(
-        DATABASE_ID,
-        MEMBERS_ID,
-        memberId
+      await databases.updateDocument(
+        DATABASE_ID, 
+        MEMBERS_ID, 
+        memberId, 
+        {
+          role,
+        }
       );
 
-      return c.json({ data: { $id: memberId } })
+      return c.json({ data: { $id: memberId } });
     }
   );
 
