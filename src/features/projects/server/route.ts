50fb88b41a65c383f85544a81141e0f453c58ee2
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-17 23:18:52 +0530

    projects: add get API

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -1,5 +1,48 @@
+import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { HTTP_STATUS } from "@/constants/api";
+import { getMembers } from "@/features/members/types/utils";
+import { sessionMiddleware } from "@/middlewares/session";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { Query } from "node-appwrite";
+import { z } from "zod";
 
-const app = new Hono();
+const app = new Hono()
+  .get(
+    "/",
+    sessionMiddleware,
+    zValidator("query", z.object({ workspaceId: z.string() })),
+    async (c) => {
+      const user = c.get("user");
+      const databases = c.get("databases");
+
+      const { workspaceId } = c.req.valid("query");
+
+      const member = await getMembers({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json(
+          { error: HTTP_STATUS.UNAUTHORISED.MESSAGE },
+          HTTP_STATUS.UNAUTHORISED.STATUS
+        );
+      }
+
+      const projects = await databases.listDocuments(
+        DATABASE_ID,
+        PROJECTS_ID,
+        [
+          Query.equal("workspaceId", workspaceId),
+          Query.orderDesc("$createdAt")
+        ]
+      );
+
+      return projects;
+    }
+  )
+;
 
 export default app;
