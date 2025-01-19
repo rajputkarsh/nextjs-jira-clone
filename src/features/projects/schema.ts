380d122557895820432d19c90e91b4c1bef0f45e
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-19 21:19:17 +0530

    projects: add createProjectSchema

--- src/features/projects/schema.ts
+++ src/features/projects/schema.ts
@@ -1,12 +1,16 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const getProjectsListSchema = z.object({ workspaceId: z.string().trim() });
+export const getProjectsListSchema = z.object({
+  workspaceId: z.string().trim(),
+});
 
 export const createProjectSchema = z.object({
   name: z.string().trim().min(1, "Required"),
   workspaceId: z.string(),
-  image: z.union([
-    z.instanceof(File),
-    z.string().transform((value) => value === "" ? undefined : value)
-  ]).optional(),
+  image: z
+    .union([
+      z.instanceof(File),
+      z.string().transform((value) => (value === "" ? undefined : value)),
+    ])
+    .optional(),
 });
