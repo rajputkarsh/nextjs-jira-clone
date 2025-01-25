5b4bdabe25e486785f0a22ab20b8998b969e0932
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-26 00:15:51 +0530

    projects: add ProjectAvatar component

---
+++ src/features/projects/components/ProjectAvatar/index.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+
+interface ProjectAvatarProps {
+  name: string;
+  image?: string;
+  className?: string;
+}
+
+function ProjectAvatar({ name, image, className }: ProjectAvatarProps) {
+  if (image && image !== "undefined") {
+    return (
+      <div
+        className={cn("size-10 relative rounded-md overflow-hidden", className)}
+      >
+        <Image src={image} alt={name} fill className="object cover" />
+      </div>
+    );
+  }
+
+  return (
+    <Avatar className={cn("size-10 rounded-md", className)}>
+      <AvatarFallback className="text-white bg-blue-600 font-semibold text-lg uppercase rounded-md">
+        {name[0]}
+      </AvatarFallback>
+    </Avatar>
+  );
+}
+
+export default ProjectAvatar;
