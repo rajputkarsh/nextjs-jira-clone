9dcaddf8505d20b46182ac74bac9b0d033e1fd8e
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-26 00:32:55 +0530

    add getProjects query

---
+++ src/features/projects/queries/index.ts
@@ -0,0 +1,86 @@
+"use server";
+
+import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { getMembers } from "@/features/members/types/utils";
+import { Models, Query } from "node-appwrite";
+import { Workspace } from "@/features/workspaces/types";
+import { createSessionClient } from "@/lib/appwrite";
+
+interface GetWorkspaceProps {
+  workspaceId: string;
+}
+
+export const getWorkspaces = async (): Promise<
+  Models.DocumentList<Models.Document>
+> => {
+  try {
+    const { account, databases } = await createSessionClient();
+
+    const user = await account.get();
+
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal("userId", user.$id),
+    ]);
+
+    if (!members.documents.length) {
+      return { documents: [], total: 0 };
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+    const workspaces = await databases.listDocuments(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      [Query.contains("$id", workspaceIds), Query.orderDesc("$createdAt")]
+    );
+
+    return workspaces;
+  } catch (e) {
+    return { documents: [], total: 0 };
+  }
+};
+
+export const getWorkspace = async ({
+  workspaceId,
+}: GetWorkspaceProps): Promise<Workspace | null> => {
+  try {
+    const { account, databases } = await createSessionClient();
+
+    const user = await account.get();
+
+    const member = await getMembers({ databases, workspaceId, userId: user.$id })
+
+    if(!member) {
+      return null;
+    }
+
+    const workspace = await databases.getDocument<Workspace>(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      workspaceId
+    );
+
+    return workspace;
+  } catch (e) {
+    return null;
+  }
+};
+
+
+export const getWorkspaceInfo = async ({
+  workspaceId,
+}: GetWorkspaceProps): Promise<{name: string} | null> => {
+  try {
+    const { databases } = await createSessionClient();
+
+    const workspace = await databases.getDocument<Workspace>(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      workspaceId
+    );
+
+    return { name: workspace.name};
+  } catch (e) {
+    return null;
+  }
+};
