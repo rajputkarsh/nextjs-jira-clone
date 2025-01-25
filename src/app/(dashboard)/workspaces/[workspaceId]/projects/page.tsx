bda1e57254487e702aa2076169919d05216b420b
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-26 00:37:13 +0530

    add project homepage

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/page.tsx
@@ -0,0 +1,33 @@
+import { getCurrent } from "@/features/auth/queries";
+import { getProjects } from "@/features/projects/queries";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface ProjectProps {
+  params: {
+    workspaceId: string;
+    projectId: string
+  };
+}
+
+export default async function Projects({ params: { workspaceId, projectId } }: ProjectProps) {
+  const user = await getCurrent();
+  if (!user) {
+    const headersList = headers();
+    const host = headersList.get("host") || "";
+    const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+
+    // Construct the current URL
+    const currentUrl = `${protocol}://${host}/projects`;
+    const encodedCallbackUrl = encodeURIComponent(currentUrl);
+    redirect(`/sign-in?callbackUrl=${encodedCallbackUrl}`);
+  }
+
+  const projects = await getProjects(workspaceId);
+
+  if (projects.total === 0) {
+    redirect("/projects/create");
+  } else {
+    redirect(`/projects/${projects.documents?.[0]?.$id}`);
+  }
+}
