431939ab70ab1d598b20f6249e3dd386aefd830d
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-26 16:30:17 +0530

    add project setting page

---
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/settings/page.tsx
@@ -0,0 +1,40 @@
+import { redirect } from "next/navigation";
+import { getCurrent } from "@/features/auth/queries";
+import UpdateProjectForm from "@/features/projects/components/UpdateProjectForm";
+import { getProject } from "@/features/projects/queries";
+import { headers } from "next/headers";
+
+interface ProjectSettingsProps {
+  params: {
+    projectId: string
+  }
+}
+
+async function ProjectSettings({ params }: ProjectSettingsProps) {
+
+  const user = await getCurrent();
+  if (!user) {
+    const headersList = headers();
+    const host = headersList.get("host") || "";
+    const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+
+    // Construct the current URL
+    const currentUrl = `${protocol}://${host}/projects/${params.projectId}/settings`;
+    const encodedCallbackUrl = encodeURIComponent(currentUrl);
+    redirect(`/sign-in?callbackUrl=${encodedCallbackUrl}`);
+  }
+
+  const initialValues = await getProject({ projectId: params.projectId });
+
+  if (!initialValues) {
+    redirect(`/projects/${params.projectId}`);
+  }
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <UpdateProjectForm initialValues={initialValues} />
+    </div>
+  );
+}
+
+export default ProjectSettings
