eb8d94e3f35c16c56b375109964d2d99ac5535fd
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-26 16:17:03 +0530

    add updateProjectSchema schema

--- src/features/projects/schema.ts
+++ src/features/projects/schema.ts
@@ -26,3 +26,13 @@ export const createProjectFormDefaultValues: ICreateProject = {
   image: "",
   workspaceId: "",
 };
+
+export const updateProjectSchema = z.object({
+  name: z.string().trim().min(1, "Must be 1 or more characters").optional(),
+  image: z
+    .union([
+      z.instanceof(File),
+      z.string().transform((value) => (value === "" ? undefined : value)),
+    ])
+    .optional(),
+});
