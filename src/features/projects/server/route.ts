17b9661001e50b94c54fcce845a87f051d561b31
utkarsh <r.utkarsh.0010@gmail.com>
2025-01-26 16:17:12 +0530

    add update-project API

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -2,10 +2,11 @@ import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config";
 import { HTTP_STATUS } from "@/constants/api";
 import { getMembers } from "@/features/members/types/utils";
 import { sessionMiddleware } from "@/middlewares/session";
-import { createProjectSchema, getProjectsListSchema } from "@/features/projects/schema";
+import { createProjectSchema, getProjectsListSchema, updateProjectSchema } from "@/features/projects/schema";
 import { Hono } from "hono";
 import { ID, Query } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
+import { Project } from "@/features/projects/types";
 
 const app = new Hono()
   .get(
@@ -96,6 +97,78 @@ const app = new Hono()
       );
 
       return c.json({ data: project });      
+    }
+  )
+  .patch(
+    "/:projectId",
+    zValidator("form", updateProjectSchema),
+    sessionMiddleware,
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
+
+      const { projectId } = c.req.param();
+      const { name, image } = c.req.valid("form");
+
+      const existingProject = await databases.getDocument<Project>(
+        DATABASE_ID,
+        PROJECTS_ID,
+        projectId
+      );
+
+      if(!existingProject) {
+        return c.json(
+          { error: HTTP_STATUS.UNAUTHORISED.MESSAGE },
+          HTTP_STATUS.UNAUTHORISED.STATUS
+        );        
+      }
+
+      const member = await getMembers({
+        databases,
+        workspaceId: existingProject.workspaceId,
+        userId: user?.$id,
+      });
+
+      if (!member) {
+        return c.json(
+          { error: HTTP_STATUS.UNAUTHORISED.MESSAGE },
+          HTTP_STATUS.UNAUTHORISED.STATUS
+        );
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`;
+      } else {
+        uploadedImageUrl = image;
+      }
+
+      const project = await databases.updateDocument(
+        DATABASE_ID,
+        PROJECTS_ID,
+        projectId,
+        {
+          name,
+          imageUrl: uploadedImageUrl,
+        }
+      );
+
+      return c.json({ data: project });
     }
   );
 export default app;
