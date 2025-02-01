ea2beb6a32f03d0abb69b5abf3cf969fa0c29429
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-02 00:11:28 +0530

    Tasks: Add TaskViewSwitcher tabs

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -6,6 +6,21 @@ function TaskViewSwitcher() {
 
   return (
     <Tabs className="flex-1 w-full rounded-lg">
+      <div className="h-full flex flex-col overflow-auto p-4">
+        <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
+          <TabsList className="w-full lg:w-auto">
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="table">
+              {translations("table")}
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="kanban">
+              {translations("kanban")}
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="calendar">
+              {translations("calendar")}
+            </TabsTrigger>
+          </TabsList>
+        </div>
+      </div>
     </Tabs>
   );
 }
