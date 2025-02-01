162ae11c90017b6d4004dc569d1e702d51f5e840
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-02 00:12:25 +0530

    Tasks: update css

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -5,7 +5,7 @@ function TaskViewSwitcher() {
   const translations = useTranslations("TaskViewSwitcher");
 
   return (
-    <Tabs className="flex-1 w-full rounded-lg">
+    <Tabs className="flex-1 w-full border rounded-lg">
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
