9901ca1c71a39b03e743ecb88198866d95c6c255
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-12 02:07:55 +0530

    Tasks: add new constant objects

--- src/features/tasks/constants/index.ts
+++ src/features/tasks/constants/index.ts
@@ -6,3 +6,26 @@ export enum TASK_STATUS {
   IN_REVIEW = "IN_REVIEW",
   DONE = "DONE",
 };
+
+export const TASK_STATUS_OBJECT: Record<TASK_STATUS, { key: TASK_STATUS; message: string }> = {
+  [TASK_STATUS.BACKLOG]: {
+    key: TASK_STATUS.BACKLOG,
+    message: "Backlog"
+  },
+  [TASK_STATUS.TODO]: {
+    key: TASK_STATUS.TODO,
+    message: "To-Do"
+  },
+  [TASK_STATUS.IN_PROGRESS]: {
+    key: TASK_STATUS.IN_PROGRESS,
+    message: "In Progress"
+  },
+  [TASK_STATUS.IN_REVIEW]: {
+    key: TASK_STATUS.IN_REVIEW,
+    message: "In Review"
+  },
+  [TASK_STATUS.DONE]: {
+    key: TASK_STATUS.DONE,
+    message: "Done"
+  },
+};
