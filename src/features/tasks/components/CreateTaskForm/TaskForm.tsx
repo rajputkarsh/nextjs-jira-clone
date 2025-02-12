715d086cbec95cafe989a9c98c97461231d04527
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-13 02:10:58 +0530

    Tasks: build create task form

--- src/features/tasks/components/CreateTaskForm/TaskForm.tsx
+++ src/features/tasks/components/CreateTaskForm/TaskForm.tsx
@@ -3,6 +3,7 @@ import { useTranslations } from "next-intl";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import DatePicker from "@/components/date-picker";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Form,
@@ -12,6 +13,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import {
   createTaskSchema,
 } from "@/features/tasks/schema";
@@ -19,8 +21,11 @@ import { DottedSeparator } from "@/components/dotter-separator";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useCreateTask } from "@/features/tasks/api/use-createTask";
-import { cn } from "@/lib/utils";
+import { capitalCase, cn } from "@/lib/utils";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspaceId";
+import MemberAvatar from "@/features/members/components/MemberAvatar";
+import { TASK_STATUS_OBJECT } from "@/features/tasks/constants";
+import ProjectAvatar from "@/features/projects/components/ProjectAvatar";
 
 interface ITaskFormProps {
   onCancel?: () => void;
@@ -84,6 +89,136 @@ function TaskForm({ onCancel, projectOptions, memberOptions }: ITaskFormProps) {
                         />
                       </FormControl>
                       <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="dueDate"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>{translations("due_date")}</FormLabel>
+                      <FormControl>
+                        <DatePicker
+                          {...field}
+                          placeholder={translations("select_date")}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="assigneeId"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>{translations("assignee")}</FormLabel>
+                      <Select
+                        defaultValue={field.value}
+                        onValueChange={field.onChange}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue
+                              placeholder={translations("assignee_placeholder")}
+                            />
+                          </SelectTrigger>
+                        </FormControl>
+                        <FormMessage />
+                        <SelectContent>
+                          {memberOptions.map((member) => (
+                            <SelectItem key={member.id} value={member.id}>
+                              <div className="flex items-center gap-x-2">
+                                <MemberAvatar
+                                  className="size-6"
+                                  name={member.name}
+                                />
+                                {capitalCase(member.name)}
+                              </div>
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="assigneeId"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>{translations("status")}</FormLabel>
+                      <Select
+                        defaultValue={field.value}
+                        onValueChange={field.onChange}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue
+                              placeholder={translations("status_placeholder")}
+                            />
+                          </SelectTrigger>
+                        </FormControl>
+                        <FormMessage />
+                        <SelectContent>
+                          {Object.values(TASK_STATUS_OBJECT).map(
+                            (taskStatus) => (
+                              <SelectItem
+                                key={taskStatus.key}
+                                value={taskStatus.key}
+                              >
+                                <div className="flex items-center gap-x-2">
+                                  {taskStatus.message}
+                                </div>
+                              </SelectItem>
+                            )
+                          )}
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="projectId"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>{translations("project")}</FormLabel>
+                      <Select
+                        defaultValue={field.value}
+                        onValueChange={field.onChange}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue
+                              placeholder={translations("project_placeholder")}
+                            />
+                          </SelectTrigger>
+                        </FormControl>
+                        <FormMessage />
+                        <SelectContent>
+                          {projectOptions.map((project) => (
+                            <SelectItem key={project.id} value={project.id}>
+                              <div className="flex items-center gap-x-2">
+                                <ProjectAvatar image={project.imageUrl}
+                                  className="size-6"
+                                  name={project.name}
+                                />
+                                {project.name}
+                              </div>
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
                     </FormItem>
                   );
                 }}
