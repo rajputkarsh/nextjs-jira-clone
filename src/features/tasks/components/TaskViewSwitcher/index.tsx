479237f39b80b3971b8de971c1f4720fbc665024
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-14 19:29:47 +0530

    chore: update string literals with enums

--- src/features/tasks/components/TaskViewSwitcher/index.tsx
+++ src/features/tasks/components/TaskViewSwitcher/index.tsx
@@ -5,25 +5,41 @@ import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { PlusIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
-import { useCreateTaskModal } from "../../hooks/use-createTaskModal";
+import { useCreateTaskModal } from "@/features/tasks/hooks/use-createTaskModal";
+import { useGetTasks } from "@/features/tasks/api/use-getTasks";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspaceId";
+
+enum AVAILABLE_TABS {
+  TABLE = "TABLE",
+  KANBAN = "KANBAN",
+  CALENDAR = "CALENDAR",
+}
 
 function TaskViewSwitcher() {
   const translations = useTranslations("TaskViewSwitcher");
-    const { open } = useCreateTaskModal();
+  const workspaceId = useWorkspaceId();
+  const { open } = useCreateTaskModal();
+
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+  });
 
   return (
-    <Tabs className="flex-1 w-full border rounded-lg">
+    <Tabs defaultValue={AVAILABLE_TABS.TABLE} className="flex-1 w-full border rounded-lg">
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
-            <TabsTrigger className="h-8 w-full lg:w-auto" value="table">
-              {translations("table")}
+            <TabsTrigger
+              className="h-8 w-full lg:w-auto"
+              value={AVAILABLE_TABS.TABLE}
+            >
+              {translations(AVAILABLE_TABS.TABLE.toLowerCase())}
             </TabsTrigger>
-            <TabsTrigger className="h-8 w-full lg:w-auto" value="kanban">
-              {translations("kanban")}
+            <TabsTrigger className="h-8 w-full lg:w-auto" value={AVAILABLE_TABS.KANBAN}>
+              {translations(AVAILABLE_TABS.KANBAN.toLowerCase())}
             </TabsTrigger>
-            <TabsTrigger className="h-8 w-full lg:w-auto" value="calendar">
-              {translations("calendar")}
+            <TabsTrigger className="h-8 w-full lg:w-auto" value={AVAILABLE_TABS.CALENDAR}>
+              {translations(AVAILABLE_TABS.CALENDAR.toLowerCase())}
             </TabsTrigger>
           </TabsList>
           <Button size={"sm"} onClick={open} className="w-full lg:w-auto">
@@ -35,13 +51,13 @@ function TaskViewSwitcher() {
         Data Filters
         <DottedSeparator className="my-4" />
         <>
-          <TabsContent value="table" className="mt-0">
+          <TabsContent value={AVAILABLE_TABS.TABLE} className="mt-0">
             Data Table
           </TabsContent>
-          <TabsContent value="kanban" className="mt-0">
+          <TabsContent value={AVAILABLE_TABS.KANBAN} className="mt-0">
             Data Kanban
           </TabsContent>
-          <TabsContent value="calendar" className="mt-0">
+          <TabsContent value={AVAILABLE_TABS.CALENDAR} className="mt-0">
             Data Calendar
           </TabsContent>
         </>
