8086716df6d716a2d0b02ab0d414efbbe3d649c8
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-16 00:05:45 +0530

    Tasks: add assigneeId filter in DataFilters

--- src/features/tasks/components/DataFilters/index.tsx
+++ src/features/tasks/components/DataFilters/index.tsx
@@ -6,6 +6,7 @@ import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspaceId";
 import { TASK_STATUS, TASK_STATUS_OBJECT } from "@/features/tasks/constants";
 import { useTaskFilters } from "@/features/tasks/hooks/use-taskFilters";
 
+import MemberAvatar from "@/features/members/components/MemberAvatar";
 import DatePicker from "@/components/date-picker";
 import {
   Select,
@@ -15,6 +16,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { capitalCase } from "@/lib/utils";
 
 interface DataFiltersProps {
   hideProjectFilter?: boolean;
@@ -48,14 +50,20 @@ function DataFilters({ hideProjectFilter }: DataFiltersProps) {
     useTaskFilters();
 
   const onStatusChange = (value: string) => {
-    setFilters({ status: value === "all" ? null: value as TASK_STATUS });
-  }
+    setFilters({ status: value === "all" ? null : (value as TASK_STATUS) });
+  };
+  const onAssigneeChange = (value: string) => {
+    setFilters({ assigneeId: value === "all" ? null : (value as string) });
+  };
 
   if (isLoading) return null;
 
   return (
     <div className="flex flex-col lg:flex-row gap-2">
-      <Select defaultValue={status || undefined} onValueChange={(value) => onStatusChange(value)}>
+      <Select
+        defaultValue={status || undefined}
+        onValueChange={(value) => onStatusChange(value)}
+      >
         <SelectTrigger className="w-full lg:w-auto h-8">
           <div className="flex items-center pr-2">
             <ListChecks className="size-4 mr-2" />
@@ -69,6 +77,30 @@ function DataFilters({ hideProjectFilter }: DataFiltersProps) {
             <SelectItem key={taskStatus.key} value={taskStatus.key}>
               <div className="flex items-center gap-x-2">
                 {taskStatus.message}
+              </div>
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      <Select
+        defaultValue={assigneeId || undefined}
+        onValueChange={(value) => onAssigneeChange(value)}
+      >
+        <SelectTrigger className="w-full lg:w-auto h-8">
+          <div className="flex items-center pr-2">
+            <ListChecks className="size-4 mr-2" />
+            <SelectValue placeholder={translations("all_assignees")} />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">{translations("all_assignees")}</SelectItem>
+          <SelectSeparator />
+          {memberOptions.map((member) => (
+            <SelectItem key={member.id} value={member.id}>
+              <div className="flex items-center gap-x-2">
+                <MemberAvatar className="size-6" name={member.name} />
+                {capitalCase(member.name)}
               </div>
             </SelectItem>
           ))}
