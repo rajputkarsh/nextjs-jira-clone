7729568b598a4080246b22ee7eed6a81f390f300
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-22 12:31:39 +0530

    Tasks: add delete task functionality

--- src/features/tasks/components/TaskActions/index.tsx
+++ src/features/tasks/components/TaskActions/index.tsx
@@ -7,6 +7,8 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import useConfirm from "@/hooks/use-confirm";
+import { useDeleteTask } from "@/features/tasks/api/use-deleteTask";
 
 interface TaskActionsProps {
   id: string;
@@ -16,39 +18,57 @@ interface TaskActionsProps {
 
 function TaskActions({ id, projectId, children }: TaskActionsProps) {
   const translations = useTranslations("TaskActions");
+  
+  const [DeleteDialog, confirmDelete] = useConfirm(
+    translations("delete_task"),
+    translations("this_action_cannot_be_undone"),
+    "destructive"
+  );
+  const { mutate: deleteTask, isPending: isDeletingTask } = useDeleteTask();
+
+  const handleDelete = async () => {
+    const ok = await confirmDelete();
+    if(!ok) return null;
+
+    deleteTask({param: { taskId: id }});
+  }
+
+  const isPending = isDeletingTask;
+
   return (
     <div className="flex justify-end">
+      <DeleteDialog />
       <DropdownMenu modal={false}>
         <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
         <DropdownMenuContent align="end" className="w-48">
           <DropdownMenuItem
-            className="font-medium p-[10px]"
-            disabled={false}
+            className="cursor-pointer font-medium p-[10px]"
+            disabled={isPending}
             onClick={() => {}}
           >
             <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
             {translations("task_details")}
           </DropdownMenuItem>
           <DropdownMenuItem
-            className="font-medium p-[10px]"
-            disabled={false}
+            className="cursor-pointer font-medium p-[10px]"
+            disabled={isPending}
             onClick={() => {}}
           >
             <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
             {translations("open_project")}
           </DropdownMenuItem>
           <DropdownMenuItem
-            className="font-medium p-[10px]"
-            disabled={false}
+            className="cursor-pointer font-medium p-[10px]"
+            disabled={isPending}
             onClick={() => {}}
           >
             <PencilIcon className="size-4 mr-2 stroke-2" />
             {translations("edit_task")}
           </DropdownMenuItem>
           <DropdownMenuItem
-            className="text-amber-700 focus:text-amber-700 font-medium p-[10px]"
-            disabled={false}
-            onClick={() => {}}
+            className="cursor-pointer text-amber-700 focus:text-amber-700 font-medium p-[10px]"
+            disabled={isPending}
+            onClick={handleDelete}
           >
             <TrashIcon className="size-4 mr-2 stroke-2" />
             {translations("delete_task")}
