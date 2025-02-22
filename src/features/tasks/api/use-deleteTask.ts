dc88d64f9f9c433cdeb62a98a4be2bf014f34178
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-22 12:20:41 +0530

    Tasks: add delete task API

---
+++ src/features/tasks/api/use-deleteTask.ts
@@ -0,0 +1,41 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { useTranslations } from "next-intl";
+import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
+
+type ResponseType = InferResponseType<typeof client.api.tasks[":taskId"]['$delete'], 200>;
+type RequestType = InferRequestType<
+  (typeof client.api.tasks)[":taskId"]["$delete"]
+>;
+
+export const useDeleteTask = () => {
+
+  const router = useRouter();
+  const translations = useTranslations("TaskActions");
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param }) => {
+      const response = await client.api.tasks[":taskId"]["$delete"]({ param });
+
+      if (!response.ok) {
+        throw new Error(translations("failed_to_delete_task"));
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success(translations("task_deleted"));
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      queryClient.invalidateQueries({ queryKey: ["tasks", data.$id] });
+      router.refresh();
+    },
+    onError: () => {
+      toast.error(translations("failed_to_delete_task"));
+    }
+  });
+
+  return mutation;
+}
