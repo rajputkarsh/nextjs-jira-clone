c8b90567c88975b4c1fb8ccb559b2bd66bd93ed3
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-22 12:15:54 +0530

    Tasks: add delete task API route

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -168,6 +168,45 @@ const app = new Hono()
 
       return c.json({ data: task });
     }
+  )
+  .delete(
+    "/:taskId",
+    sessionMiddleware,
+    async (c) => {
+      const user = c.get("user");
+      const databases = c.get("databases");
+
+      const { taskId } = c.req.param();
+
+      const task = await databases.getDocument<Task>(
+        DATABASE_ID,
+        TASKS_ID,
+        taskId
+      );
+
+      const member = await getMembers({
+        databases,
+        workspaceId: task.workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json(
+          { error: HTTP_STATUS.UNAUTHORISED.MESSAGE },
+          HTTP_STATUS.UNAUTHORISED.STATUS
+        );
+      }
+
+      await databases.deleteDocument(
+        DATABASE_ID,
+        TASKS_ID,
+        taskId
+      );
+
+      return c.json({ data: { $id: task.$id } });
+
+
+    }
   );
 
 export default app;
