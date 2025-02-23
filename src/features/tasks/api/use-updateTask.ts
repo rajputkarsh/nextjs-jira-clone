e53cdfc765bcc3a0a9962df78107d8668f9b5565
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-23 15:16:36 +0530

    Tasks: add useUpdateTask API

---
+++ src/features/tasks/api/use-updateTask.ts
@@ -0,0 +1,40 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { useTranslations } from "next-intl";
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<typeof client.api.tasks[":taskId"]['$patch'], 200>;
+type RequestType = InferRequestType<
+  (typeof client.api.tasks)[":taskId"]["$patch"]
+>;
+
+export const useUpdateTask = () => {
+
+  const translations = useTranslations("TaskActions");
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param, json }) => {
+      const response = await client.api.tasks[":taskId"]["$patch"]({
+        param,
+        json,
+      });
+
+      if (!response.ok) {
+        throw new Error(translations("failed_to_update_task"));
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success(translations("task_updated"));
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    onError: () => {
+      toast.error(translations("failed_to_update_task"));
+    }
+  });
+
+  return mutation;
+}
