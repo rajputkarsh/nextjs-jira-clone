872cf87b3e5de4e29b1769db68ea1ca7e9f1bae7
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-26 14:31:09 +0530

    Tasks: add KanbanCard component

--- src/features/tasks/components/DataKanban/KanbanCard.tsx
+++ src/features/tasks/components/DataKanban/KanbanCard.tsx
@@ -10,7 +10,7 @@ function KanbanCard({ task }: KanbanCardProps) {
   return (
     <div className="bg-white p-2.5 mb-1.5 rounded shadow-sm space-y-3">
       <div className="flex items-start justify-between gap-x-2">
-        <p>{task.name}</p>
+        <p className="text-sm line-clamp-2">{task.name}</p>
         <TaskActions id={task.$id as string} projectId={task.projectId}>
           <MoreHorizontalIcon className="size-[18px] stroke-1 shrink-0 text-neutral-700 hover:opacity-75 transition" />
         </TaskActions>
