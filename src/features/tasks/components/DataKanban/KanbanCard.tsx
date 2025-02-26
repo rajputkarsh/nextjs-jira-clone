c78c98c1d28f75179b174d840ea9e8c66fea47d0
utkarsh <r.utkarsh.0010@gmail.com>
2025-02-26 14:30:06 +0530

    Tasks: add KanbanCard component

---
+++ src/features/tasks/components/DataKanban/KanbanCard.tsx
@@ -0,0 +1,22 @@
+import TaskActions from "@/features/tasks/components/TaskActions";
+import { ITask } from "@/features/tasks/schema";
+import { MoreHorizontalIcon } from "lucide-react";
+
+interface KanbanCardProps {
+  task: ITask
+}
+
+function KanbanCard({ task }: KanbanCardProps) {
+  return (
+    <div className="bg-white p-2.5 mb-1.5 rounded shadow-sm space-y-3">
+      <div className="flex items-start justify-between gap-x-2">
+        <p>{task.name}</p>
+        <TaskActions id={task.$id as string} projectId={task.projectId}>
+          <MoreHorizontalIcon className="size-[18px] stroke-1 shrink-0 text-neutral-700 hover:opacity-75 transition" />
+        </TaskActions>
+      </div>
+    </div>
+  );
+}
+
+export default KanbanCard
