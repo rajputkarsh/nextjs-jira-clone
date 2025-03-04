b9f0f1bc5741fce02096c6b671190fc084c690db
utkarsh <r.utkarsh.0010@gmail.com>
2025-03-04 17:12:36 +0530

    Tasks: add bulk-update API

---
+++ src/features/tasks/api/use-bulkUpdateTasks.ts
@@ -0,0 +1,44 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { useTranslations } from "next-intl";
+import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.tasks)["bulk-update"]["$post"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.tasks)["bulk-update"]["$post"]
+>;
+
+export const useBulkUpdateTasks = () => {
+  const router = useRouter();
+  const translations = useTranslations("TaskActions");
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ json }) => {
+      const response = await client.api.tasks["bulk-update"]["$post"]({
+        json,
+      });
+
+      if (!response.ok) {
+        throw new Error(translations("failed_to_update_task"));
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success(translations("task_updated"));
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      router.refresh();
+    },
+    onError: () => {
+      toast.error(translations("failed_to_update_task"));
+    },
+  });
+
+  return mutation;
+};
