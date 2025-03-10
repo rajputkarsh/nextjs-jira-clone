3b11c646be5600e38d37409e785d60b818cab760
utkarsh <r.utkarsh.0010@gmail.com>
2025-03-11 00:22:11 +0530

    chore: add client side page error

---
+++ src/components/page-error.tsx
@@ -0,0 +1,16 @@
+import { AlertTriangle } from "lucide-react";
+
+interface PageErrorProps {
+  message: string;
+}
+
+function PageError({ message }: PageErrorProps) {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <AlertTriangle className="size-6 mb-2 text-muted-foreground" />
+      <p className="text-sm font-medium text-muted-foreground">{message}</p>
+    </div>
+  );
+}
+
+export default PageError;
