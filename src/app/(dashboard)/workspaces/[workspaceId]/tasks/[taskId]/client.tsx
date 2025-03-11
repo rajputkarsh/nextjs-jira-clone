bc2eedee37f36282d53f37da04be7756fdac07ed
utkarsh <r.utkarsh.0010@gmail.com>
2025-03-12 00:26:21 +0530

    Tasks: add task details route

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,9 +1,26 @@
 "use client";
 
+import PageError from "@/components/page-error";
+import PageLoader from "@/components/page-loader";
+import { useGetTasksById } from "@/features/tasks/api/use-getTaskById";
+import { useTaskId } from "@/features/tasks/hooks/use-taskId";
+import { useTranslations } from "next-intl";
+
 function TaskClientPage() {
-  return (
-    <div>TaskClientPage</div>
-  )
+  const taskId = useTaskId();
+  const translate = useTranslations("task");
+
+  const { data, isLoading } = useGetTasksById({ taskId });
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
+
+  if (!data) {
+    return <PageError message={translate("task_not_found")} />;
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
 }
 
-export default TaskClientPage
+export default TaskClientPage;
