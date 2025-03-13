8fde3f37f32736462943e2706c2ab578d9af5284
utkarsh <r.utkarsh.0010@gmail.com>
2025-03-14 00:45:06 +0530

    Task Info: add delete functionality in task breadcrumbs

--- src/features/tasks/components/TaskBreadcrumbs/index.tsx
+++ src/features/tasks/components/TaskBreadcrumbs/index.tsx
@@ -1,25 +1,49 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
+import { useDeleteTask } from "@/features/tasks/api/use-deleteTask";
 import ProjectAvatar from "@/features/projects/components/ProjectAvatar";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 import { Project } from "@/features/projects/types";
+import { Button } from "@/components/ui/button";
 import { Task } from "@/features/tasks/schema";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspaceId";
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
-import { useTranslations } from "next-intl";
-import Link from "next/link";
+import useConfirm from "@/hooks/use-confirm";
 
 interface TaskBreadcrumbsProps {
   project: Project;
-  task: Task
+  task: Task;
 }
 
 function TaskBreadcrumbs({ project, task }: TaskBreadcrumbsProps) {
   const translate = useTranslations("TaskActions");
   const workspaceId = useWorkspaceId();
+  const router = useRouter();
+
+  const { mutate: deleteTask, isPending } = useDeleteTask();
+  const [ConfirmDialog, confirm] = useConfirm(
+    translate("delete_task"),
+    translate("this_action_cannot_be_undone"),
+    "destructive"
+  );
+
+  const handleDelete = async () => {
+
+    const ok = await confirm();
+    if (!ok) return;
+
+    deleteTask({ param: { taskId: task.$id } }, {
+      onSuccess: () => {
+        router.push(`/workspaces/${workspaceId}/tasks`);
+      }
+    });
+  };
 
   return (
     <div className="flex items-center gap-x-2">
+      <ConfirmDialog />
       <ProjectAvatar
         name={project?.name}
         image={project?.imageUrl}
@@ -32,7 +56,13 @@ function TaskBreadcrumbs({ project, task }: TaskBreadcrumbsProps) {
       </Link>
       <ChevronRightIcon className="size-4 lg:size-5 text-muted-foreground" />
       <p className="text-sm lg:text-lg font-semibold">{task?.name}</p>
-      <Button className="ml-auto" variant={"destructive"} size="sm">
+      <Button
+        onClick={handleDelete}
+        disabled={isPending}
+        className="ml-auto"
+        variant={"destructive"}
+        size="sm"
+      >
         <TrashIcon className="size-4 lg:mr-2" />
         <span className="hidden lg:block">{translate("delete_task")}</span>
       </Button>
