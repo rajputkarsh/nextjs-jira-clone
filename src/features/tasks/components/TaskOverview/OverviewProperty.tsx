7a24463dbda85b410ec10634f0db9f14cbb358c1
utkarsh <r.utkarsh.0010@gmail.com>
2025-03-15 01:12:01 +0530

    add OverviewProperty component

---
+++ src/features/tasks/components/TaskOverview/OverviewProperty.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+interface OverviewPropertyProps {
+  label: string;
+  children: ReactNode
+}
+
+function OverviewProperty({ label, children }: OverviewPropertyProps) {
+  return (
+    <div className="flex items-start gap-x-2">
+      <div className="min-w-[100px]">
+        <p className="text-sm text-muted-foreground">{label}</p>
+      </div>
+      <div className="flex items-center gap-x-2">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default OverviewProperty
