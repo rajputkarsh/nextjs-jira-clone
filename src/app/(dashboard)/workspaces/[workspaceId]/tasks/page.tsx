18588a68ec5630e09b70ae48f49abd37a450496a
utkarsh <r.utkarsh.0010@gmail.com>
2025-03-14 23:07:07 +0530

    add my-tasks page

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/page.tsx
@@ -0,0 +1,27 @@
+import { getCurrent } from "@/features/auth/queries";
+import TaskViewSwitcher from "@/features/tasks/components/TaskViewSwitcher";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface TaskProps {
+  params: {
+    workspaceId: string;
+    projectId: string;
+  };
+}
+
+export default async function Tasks({ params: { workspaceId } }: TaskProps) {
+  const user = await getCurrent();
+  if (!user) {
+    const headersList = headers();
+    const host = headersList.get("host") || "";
+    const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+
+    // Construct the current URL
+    const currentUrl = `${protocol}://${host}/workspaces/${workspaceId}/tasks`;
+    const encodedCallbackUrl = encodeURIComponent(currentUrl);
+    redirect(`/sign-in?callbackUrl=${encodedCallbackUrl}`);
+  }
+
+  return <TaskViewSwitcher />;
+}
