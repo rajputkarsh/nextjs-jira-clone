f0b489b1f600fc32752c544d41aafa85ce4dab6b
utkarsh <r.utkarsh.0010@gmail.com>
2025-03-17 04:17:44 +0530

    Projects: add Project Analytics UI

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import AnalyticsCard from "@/components/analytics-card";
 import { ResponseType as ProjectAnalyticsResponseType } from "@/features/projects/api/use-getProjectAnalyticsById";
+import { DottedSeparator } from "./dotter-separator";
 
 interface AnalyticsProps extends ProjectAnalyticsResponseType {
-  
+
 };
 
 function Analytics({ data }: AnalyticsProps) {
@@ -27,8 +28,45 @@ function Analytics({ data }: AnalyticsProps) {
             variant={data.taskDifference > 0 ? "up" : "down"}
             increaseValue={data.taskDifference}
           />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title={translate("assigned_tasks")}
+            value={data.assignedTaskCount}
+            variant={data.assignedTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.assignedTaskCount}
+          />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title={translate("completed_tasks")}
+            value={data.completedTaskCount}
+            variant={data.completedTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.completedTaskCount}
+          />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title={translate("overdue_tasks")}
+            value={data.overdueTaskCount}
+            variant={data.overdueTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.overdueTaskCount}
+          />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex items-center flex-1">
+          <AnalyticsCard
+            title={translate("incomplete_tasks")}
+            value={data.incompleteTaskCount}
+            variant={data.incompleteTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.incompleteTaskCount}
+          />
         </div>
       </div>
+      <ScrollBar orientation="horizontal" />
     </ScrollArea>
   );
 }
