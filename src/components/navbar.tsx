f475b0838583d15ab76f3ad5233920dbaa36314d
utkarsh <r.utkarsh.0010@gmail.com>
2025-03-18 02:27:48 +0530

    add dynamic page title and description

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,43 @@
+"use client";
+
 import UserButton from "@/features/auth/components/UserButton";
 import { useTranslations } from "next-intl"
 import MobileSidebar from "@/components/mobile-sidebar";
+import { usePathname } from "next/navigation";
+
+const PATHNAME_MAP = {
+  home: {
+    title: "home",
+    description: "home_desc",
+  },
+  tasks: {
+    title: "tasks",
+    description: "tasks_desc",
+  },
+  projects: {
+    title: "projects",
+    description: "projects_desc",
+  },
+} as { [key: string]: { title: string, description: string } };
+
+const getTitleAndDescription = (key: string) => {
+  if (Object.keys(PATHNAME_MAP).includes(key)) return PATHNAME_MAP[key];
+  return PATHNAME_MAP["home"];
+}; 
 
 function Navbar() {
-  const translations = useTranslations("Navbar");
+  const translate = useTranslations("Navbar");
+  const pathname = usePathname();
+
+  const pathnameParts = pathname.split("/");
+
+  const { title, description } = getTitleAndDescription(pathnameParts["3"]);
+
   return (
     <nav className="pt-4 px-6 flex items-center justify-between">
       <div className="flex-col hidden lg:flex">
-        <h1 className="text-2xl font-semibold">
-          {translations("home")}
-        </h1>
-        <p className="text-muted-foreground">{translations("home_desc")}</p>
+        <h1 className="text-2xl font-semibold">{translate(title)}</h1>
+        <p className="text-muted-foreground">{translate(description)}</p>
       </div>
       <MobileSidebar />
       <UserButton />
