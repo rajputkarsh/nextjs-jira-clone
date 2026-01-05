0581ca5c3c983b89968d34f394e2835fce6e5eef
utkarsh <r.utkarsh.0010@gmail.com>
2026-01-05 19:50:31 +0530

    add comments schema

---
+++ src/features/comments/schema.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+import { Models } from "node-appwrite";
+
+export interface IComment {
+  $id?: string;
+  taskId: string;
+  userId: string;
+  message: string;
+  $createdAt?: string;
+  $updatedAt?: string;
+}
+
+export type Comment = Models.Document & IComment;
+
+export interface CommentWithUser extends Comment {
+  user: {
+    name: string;
+    email: string;
+  };
+}
+
+export const createCommentSchema = z.object({
+  taskId: z.string().trim().min(1, "Required"),
+  message: z.string().trim().min(1, "Comment cannot be empty"),
+});
+
+export const getCommentsSchema = z.object({
+  taskId: z.string().trim().min(1, "Required"),
+});
+
